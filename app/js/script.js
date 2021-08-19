9485253e1f4ca6ccf810a66b142c12f415774aca
joebalzac <jgbalzac@gmail.com>
2021-08-19 16:34:41 -0400

    add elements

--- app/js/script.js
+++ app/js/script.js
@@ -7,7 +7,11 @@ const btnClear = document.querySelector("#clear-completed");
 const themeSwitch = document.querySelector("#theme-toggle");
 const themeLogos = document.querySelectorAll(".btn--theme img");
 
-const themeSwitcher = (e) => {
+/******************************/
+/***  FUNCTIONS       ***/
+/******************************/
+
+function themeSwitcher(e) {
   console.log(e.target);
   //change the logo to sun or moon
   themeLogos.forEach((logo) => logo.classList.toggle("todo__elem--hide"));
@@ -17,6 +21,20 @@ const themeSwitcher = (e) => {
   } else {
     document.body.dataset.theme = "";
   }
-};
+}
 
 themeSwitch.addEventListener("click", themeSwitcher);
+
+todoInput.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+  }
+  if (e.target.value !== "") {
+    addTodoElem(e.target.value);
+    todoInput.value = "";
+  }
+});
+
+function addTodoElem() {
+  const todoEl = document.createElement("li");
+  todoEl.classList.add("todo__elem");
+}
